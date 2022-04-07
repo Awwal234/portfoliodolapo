ced06499b348563646b8fe3e6b58a0d1cab9f2c8
Awwal234 <90981208+Awwal234@users.noreply.github.com>
2022-04-08 00:58:37 +0100

    Add files via upload

---
+++ mill.js
@@ -0,0 +1,51 @@
+//Hello at miil.js
+//writing function to index.html
+
+
+function openMenu(){
+    document.getElementById('menu_overlay_container').style.display="block";
+}
+
+function closeMenu(){
+    document.getElementById('menu_overlay_container').style.display="none";
+}
+
+//redirect 1st hirebutton on img
+
+function Enter(){
+    window.location.href="mailto:fabowalemuhawwal@gmail.com";
+}
+
+
+function change(x){
+    x.style.backgroundColor='blue';
+    x.style.color='#fff';
+}
+function change2(y){
+    y.style.backgroundColor='transparent';
+    y.style.color='rgba(8, 8, 138, 0.808)';
+}
+
+//first change bg and color
+
+//second change bg and color
+
+function changeme(x){
+    x.style.backgroundColor='blue';
+    x.style.color='#fff';
+}
+function changeme2(y){
+    y.style.backgroundColor='transparent';
+    y.style.color='rgba(8, 8, 138, 0.808)';
+}
+
+//third change bg and color
+
+function changebox(x){
+    x.style.backgroundColor='blue';
+    x.style.color='#fff';
+}
+function changebox2(y){
+    y.style.backgroundColor='transparent';
+    y.style.color='rgba(8, 8, 138, 0.808)';
+}
